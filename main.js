989489fe2c53dc071e625b915632ea901ca8813b
Alberto Arias <alberto.arias.x64@gmail.com>
2024-08-20 11:03:14 -0500

    adding state

--- main.js
+++ main.js
@@ -1,15 +1,15 @@
 const TABLE = document.querySelector("table")
 const TABLE_HEAD = document.querySelector("thead")
 const TABLE_BODY = document.querySelector("tbody")
-const ROWS = 10
-const COLUMNS = 10
+const ROWS = 5
+const COLUMNS = 5
 
-function range(times = 0) {
-  return new Array(times).fill(0).map((_, i) => i)
-}
+const range = (times = 0) => new Array(times).fill(0).map((_, i) => i)
+
+const STATE = range(ROWS).map(i => range(COLUMNS + 1).map(j => 0))
 
 function renderTable() {
   TABLE_HEAD.innerHTML = `<tr>${range(COLUMNS + 1).map((i) => `<th>${i === 0 ? '' : String.fromCharCode(64 + i)}</th>`).join('')}</tr>`
-  TABLE_BODY.innerHTML = `${range(ROWS).map((i) => `<tr>${range(COLUMNS + 1).map((j) => `<td data-y="${i + 1}" data-x="${j}"  >${j === 0 ? i + 1 : ''}</td>`).join("")}</tr>`).join('')}`
+  TABLE_BODY.innerHTML = `${range(ROWS).map((i) => `<tr>${range(COLUMNS + 1).map((j) => `<td data-y="${i + 1}" data-x="${j}"  >${j === 0 ? i + 1 : `<span>${STATE[i][j - 1]}</span>`}</td>`).join("")}</tr>`).join('')}`
 }
 renderTable();
