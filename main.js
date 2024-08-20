797c9f54fcf585355a0fafe720888419e3c822ba
Alberto Arias <alberto.arias.x64@gmail.com>
2024-08-20 13:59:44 -0500

    adding operations

--- main.js
+++ main.js
@@ -6,11 +6,17 @@ const COLUMNS = 5
 
 const range = (times = 0) => new Array(times).fill(0).map((_, i) => i)
 
-const STATE = range(ROWS).map(i => range(COLUMNS + 1).map(j => i + j))
+const STATE = range(ROWS).map(() => range(COLUMNS + 1).map(() => ({ value: '', input: '' })))
 
 function renderTable() {
   TABLE_HEAD.innerHTML = `<tr>${range(COLUMNS + 1).map((i) => `<th>${i === 0 ? '' : String.fromCharCode(64 + i)}</th>`).join('')}</tr>`
-  TABLE_BODY.innerHTML = `${range(ROWS).map((i) => `<tr>${range(COLUMNS + 1).map((j) => `<td data-y="${i + 1}" data-x="${j}"  >${j === 0 ? i + 1 : `<span>${STATE[i][j - 1]}</span> <input type="text" value="${STATE[i][j - 1]}">`}</td>`).join("")}</tr>`).join('')}`
+  TABLE_BODY.innerHTML = `${range(ROWS).map((i) => `<tr>${range(COLUMNS + 1).map((j) => `<td data-y="${i + 1}" data-x="${j}"  >${j === 0 ? i + 1 : `<span>${STATE[i][j - 1].value}</span> <input type="text" value="${STATE[i][j - 1].input}">`}</td>`).join("")}</tr>`).join('')}`
+}
+
+function computeValue(value = '') {
+  if (!value.startsWith('=')) return value
+  const op = value.substring(1)
+  return eval(op)
 }
 
 TABLE.addEventListener("click", ({ target }) => {
@@ -24,7 +30,8 @@ TABLE.addEventListener("click", ({ target }) => {
   input.addEventListener('blur', ({ target }) => {
     const { x, y } = target.closest("td").dataset
     if (input.value === STATE[y - 1][x - 1]) return
-    STATE[y - 1][x - 1] = input.value
+    STATE[y - 1][x - 1].value = computeValue(input.value)
+    STATE[y - 1][x - 1].input = input.value
     renderTable()
   }, { once: true })
 })
