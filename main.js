12a39e3200d71f38f228d595474c3c5ca38dca3a
Alberto Arias <alberto.arias.x64@gmail.com>
2024-08-20 11:32:54 -0500

    adding selected item

--- main.js
+++ main.js
@@ -6,10 +6,17 @@ const COLUMNS = 5
 
 const range = (times = 0) => new Array(times).fill(0).map((_, i) => i)
 
-const STATE = range(ROWS).map(i => range(COLUMNS + 1).map(j => 0))
+const STATE = range(ROWS).map(i => range(COLUMNS + 1).map(j => i + j))
 
 function renderTable() {
   TABLE_HEAD.innerHTML = `<tr>${range(COLUMNS + 1).map((i) => `<th>${i === 0 ? '' : String.fromCharCode(64 + i)}</th>`).join('')}</tr>`
-  TABLE_BODY.innerHTML = `${range(ROWS).map((i) => `<tr>${range(COLUMNS + 1).map((j) => `<td data-y="${i + 1}" data-x="${j}"  >${j === 0 ? i + 1 : `<span>${STATE[i][j - 1]}</span>`}</td>`).join("")}</tr>`).join('')}`
+  TABLE_BODY.innerHTML = `${range(ROWS).map((i) => `<tr>${range(COLUMNS + 1).map((j) => `<td data-y="${i + 1}" data-x="${j}"  >${j === 0 ? i + 1 : `<span>${STATE[i][j - 1]}</span> <input type="text" value="${STATE[i][j - 1]}">`}</td>`).join("")}</tr>`).join('')}`
 }
+
+TABLE.addEventListener("click", ({ target }) => {
+  const td = target.closest("td")
+  if (!td) return
+  const { x, y } = td.dataset
+  td.classList.add("selected")
+})
 renderTable();
