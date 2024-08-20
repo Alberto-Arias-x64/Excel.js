f466d49058436d49cf5fd939bbd18e0661c5e486
Alberto Arias <alberto.arias.x64@gmail.com>
2024-08-20 10:48:41 -0500

    adding styles and improve view

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 const TABLE = document.querySelector("table")
 const TABLE_HEAD = document.querySelector("thead")
 const TABLE_BODY = document.querySelector("tbody")
-const ROWS = 20
-const COLUMNS = 20
+const ROWS = 10
+const COLUMNS = 10
 
 function range(times = 0) {
   return new Array(times).fill(0).map((_, i) => i)
@@ -10,6 +10,6 @@ function range(times = 0) {
 
 function renderTable() {
   TABLE_HEAD.innerHTML = `<tr>${range(COLUMNS + 1).map((i) => `<th>${i === 0 ? '' : String.fromCharCode(64 + i)}</th>`).join('')}</tr>`
-  TABLE_BODY.innerHTML = `${range(ROWS).map((i) => `<tr>${range(COLUMNS + 1).map((j) => `<td>${j === 0 ? i + 1 : ''}</td>`).join("")}</tr>`).join('')}`
+  TABLE_BODY.innerHTML = `${range(ROWS).map((i) => `<tr>${range(COLUMNS + 1).map((j) => `<td data-y="${i + 1}" data-x="${j}"  >${j === 0 ? i + 1 : ''}</td>`).join("")}</tr>`).join('')}`
 }
 renderTable();
