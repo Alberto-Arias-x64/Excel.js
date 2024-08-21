a9b814c87c93016ff1d785e7594319544695b251
Alberto Arias <alberto.arias.x64@gmail.com>
2024-08-21 09:39:06 -0500

    recalculate on change

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 const TABLE = document.querySelector("table")
 const TABLE_HEAD = document.querySelector("thead")
 const TABLE_BODY = document.querySelector("tbody")
-const ROWS = 5
-const COLUMNS = 5
+const ROWS = 10
+const COLUMNS = 10
 
 const range = (times = 0) => new Array(times).fill(0).map((_, i) => i)
 const STATE = range(ROWS).map(() => range(COLUMNS + 1).map(() => ({ value: '', input: '' })))
@@ -15,7 +15,7 @@ function replaceValues(value) {
 
 function renderTable() {
   TABLE_HEAD.innerHTML = `<tr>${range(COLUMNS + 1).map((i) => `<th>${i === 0 ? '' : String.fromCharCode(64 + i)}</th>`).join('')}</tr>`
-  TABLE_BODY.innerHTML = `${range(ROWS).map((i) => `<tr>${range(COLUMNS + 1).map((j) => `<td data-y="${i + 1}" data-x="${j}"  >${j === 0 ? i + 1 : `<span>${STATE[i][j - 1].value}</span> <input type="text" value="${STATE[i][j - 1].input}">`}</td>`).join("")}</tr>`).join('')}`
+  TABLE_BODY.innerHTML = `${range(ROWS).map((i) => `<tr>${range(COLUMNS + 1).map((j) => `<td data-y="${i + 1}" data-x="${j}"  >${j === 0 ? i + 1 : `<span>${STATE[i][j - 1].value}</span> <input type="text" name="${i + ' ' + j}" value="${STATE[i][j - 1].input}">`}</td>`).join("")}</tr>`).join('')}`
 }
 
 function computeValue(value = '') {
@@ -28,6 +28,15 @@ function computeValue(value = '') {
   }
 }
 
+function recalculate() {
+  STATE.forEach((row) => {
+    row.forEach((cell) => {
+      const { input } = cell
+      if (input.startsWith('=')) cell.value = computeValue(input)
+    })
+  })
+}
+
 TABLE.addEventListener("click", ({ target }) => {
   const td = target.closest("td")
   if (!td) return
@@ -36,11 +45,11 @@ TABLE.addEventListener("click", ({ target }) => {
   const end = input.value.length
   input.setSelectionRange(end, end)
   input.focus()
-  input.addEventListener('blur', ({ target }) => {
-    const { x, y } = target.closest("td").dataset
+  input.addEventListener('blur', () => {
     if (input.value === STATE[y - 1][x - 1].input) return
     STATE[y - 1][x - 1].value = computeValue(input.value)
     STATE[y - 1][x - 1].input = input.value
+    recalculate()
     renderTable()
   }, { once: true })
 })
